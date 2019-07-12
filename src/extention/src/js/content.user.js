468257d70dffab84b4fe2ce73ca2709cc618b5b9
Alexander <pichulev23071999@gmail.com>
2019-07-12 18:24:59 +0400

    Кнопка Загрузить для extention, объединение таблицы от ARWU

--- src/extention/src/js/content.user.js
+++ src/extention/src/js/content.user.js
@@ -43,7 +43,7 @@ function rangeAverage(first, last) {
 }
 
 // Разбор таблицы
-function parseTable(table, name) {
+function parseTable(table) {
   let universities = new Array;
 
   var prevRank = -1; // предыдущий ранг
@@ -114,8 +114,7 @@ function parseTable(table, name) {
     universities.push(buffer);
   });
 
-  // Загрузка массива в виде csv файла
-  downloadCSV(universities, name);
+  return universities;
 }
 
 // Главная функция
@@ -134,52 +133,162 @@ function parseTable(table, name) {
     return;
   }
 
+  // Создание кнопки загрузки
+  $('body').prepend('<div class="pulse" style="opacity: 0">' +
+    '  <img src="' + chrome.runtime.getURL('data/images/icons/download-icon.png') + '"/>' +
+    '</div>');
+  $('<style type="text/css">' +
+    '.pulse {' +
+    '  display: block;' +
+    '  border-radius: 50%;' +
+    '  background: #00c9ca;' +
+    '  cursor: pointer;' +
+    '  position: fixed;' +
+    '  top: 20px;' +
+    '  transition: all 0.2s;' +
+    '  right: 20px;' +
+    '  border: 1px solid #00c9ca;' +
+    '  z-index: 9999;' +
+    '}' +
+    '' +
+    '.pulse img {' +
+    '  border-radius: 50%;' +
+    '  width: 60px;' +
+    '  height: 60px;' +
+    '}' +
+    '' +
+    '.pulse:hover {' +
+    '  opacity: 0.7 !important;' +
+    '}' +
+    '' +
+    '.pulse:after {' +
+    '  content:"";' +
+    '  width: 64px !important;' +
+    '  height: 64px !important;' +
+    '  border-radius: 50%;' +
+    '  border: 4px solid #00c9ca;' +
+    '  position: fixed;' +
+    '  top:17px;' +
+    '  right:17px;' +
+    '  animation: pulse 2s ease 0s infinite;' +
+    '  box-sizing: border-box;' +
+    '}' +
+    '' +
+    '@keyframes pulse {' +
+    '  0% {' +
+    '    opacity: 1;' +
+    '    transform: scale(1);' +
+    '  }' +
+    '  80% {' +
+    '    opacity: 0;' +
+    '    transform: scale(2);' +
+    ' }' +
+    '  100% {' +
+    '    opacity: 0;' +
+    '    transform: scale(2.5);' +
+    '  }' +
+    '}' +
+    '</style>')
+    .appendTo('head');
+
   // Запустить только после загрузки всей страницы
   $(window).bind("load", function () {
     let currUrl = window.location.toString();
     // The World University Rankings
     if (currUrl.match("https://www.timeshighereducation.com/world-university-rankings/*")) {
-      if (confirm("Загрузить таблицу рейтингов от THE?")) { // пользователь нажал 'Ok'
+
+      // Событие нажатия кнопки загрузки
+      $(document).on('click', '.pulse', function () {
         let table = {
           headline: '.table tbody tr',
           rank: '.rank',
           name: '.name .ranking-institution-title'
         }
-        parseTable(table, 'World University Rankings (THE)');
+        // Загрузка массива в виде csv файла
+        downloadCSV(parseTable(table), 'THE');
+      });
+
+      if (confirm("Загрузить таблицу рейтингов от THE сейчас?")) { // пользователь нажал 'Ok'
+        $('.pulse').trigger('click');
+      } else { // иначе - показать кнопку
+        $('.pulse').animate({
+          opacity: 1,
+        }, 300);
       }
     } // CWUR World University Rankings 
     else if (currUrl.match("https://cwur.org/2018-19.php")) {
-      if (confirm("Загрузить таблицу рейтингов от CWUR?")) { // пользователь нажал 'Ok'
+
+      // Событие нажатия кнопки загрузки
+      $(document).on('click', '.pulse', function () {
         let table = {
           headline: '.table tbody tr',
           rank: 'td:nth-child(1)',
           name: 'td:nth-child(2) a'
         }
-        parseTable(table, 'World University Rankings (CWUR)');
+        // Загрузка массива в виде csv файла
+        downloadCSV(parseTable(table), 'CWUR');
+      });
+
+      if (confirm("Загрузить таблицу рейтингов от CWUR сейчас?")) { // пользователь нажал 'Ok'
+        $('.pulse').trigger('click');
+      } else { // иначе - показать кнопку
+        $('.pulse').animate({
+          opacity: 1,
+        }, 300);
       }
     } // RUR Round University Rankings 
     else if (currUrl.match("http://roundranking.com/ranking/world-university-rankings.html*")) {
-      if (confirm("Загрузить таблицу рейтингов от RUR?")) { // пользователь нажал 'Ok'
+
+      // Событие нажатия кнопки загрузки
+      $(document).on('click', '.pulse', function () {
         let table = {
           headline: '.big-table tbody tr',
           rank: '.td1',
           name: '.td2 a'
         }
-        parseTable(table, 'World University Rankings (RUR)');
+        // Загрузка массива в виде csv файла
+        downloadCSV(parseTable(table), 'RUR');
+      });
+
+      if (confirm("Загрузить таблицу рейтингов от RUR сейчас?")) { // пользователь нажал 'Ok'
+        $('.pulse').trigger('click');
+      } else { // иначе - показать кнопку
+        $('.pulse').animate({
+          opacity: 1,
+        }, 300);
       }
     } // Academic Ranking of World Universities
     else if (currUrl.match("http://www.shanghairanking.com/ARWU2018.html")) {
-      if (confirm("Загрузить таблицу рейтингов от ARWU?")) { // пользователь нажал 'Ok'
+
+      // Событие нажатия кнопки загрузки
+      $(document).on('click', '.pulse', function () {
         let table = {
           headline: '#UniversityRanking tbody tr:not(:first)',
           rank: 'td:nth-child(1)',
           name: 'td:nth-child(2) a'
         }
-        parseTable(table, 'World University Rankings (ARWU)');
+        // Разбор таблицы
+        let universities = parseTable(table);
+        // Сохранение таблицы
+        chrome.storage.local.set({
+          keptTable: universities
+        }, function () { // переход на страницу с продолжением таблицы
+          document.location.href = "http://www.shanghairanking.com/ARWU2018Candidates.html";
+        });
+      });
+
+      if (confirm("Загрузить таблицу рейтингов от ARWU сейчас?")) { // пользователь нажал 'Ok'
+        $('.pulse').trigger('click');
+      } else { // иначе - показать кнопку
+        $('.pulse').animate({
+          opacity: 1,
+        }, 300);
       }
     } // Academic Ranking of World Universities (second part)
     else if (currUrl.match("http://www.shanghairanking.com/ARWU2018Candidates.html")) {
-      if (confirm("Загрузить таблицу рейтингов от ARWU?")) { // пользователь нажал 'Ok'
+
+      // Событие нажатия кнопки загрузки
+      $(document).on('click', '.pulse', function () {
         let universities = new Array;
         $('#UniversityRanking tbody tr').each(function (i, el) {
           if (i) {
@@ -200,7 +309,26 @@ function parseTable(table, name) {
             universities.push(buffer);
           }
         });
-        downloadCSV(universities, 'World University Rankings (ARWU)');
+        // Проверка на наличие сохраненной таблицы
+        chrome.storage.local.get(['keptTable'], function (result) {
+          if (result.keptTable === undefined) { // загрузить текущую таблицу
+            downloadCSV(universities, 'ARWU');
+          } else { // объединить текущую таблицу с предыдущей
+            universities = result.keptTable.concat(universities);
+            // Загрузка таблицы
+            downloadCSV(universities, 'ARWU');
+            // Освобождение памяти
+            chrome.storage.local.remove(['keptTable'], function () { });
+          }
+        });
+      });
+
+      if (confirm("Загрузить таблицу рейтингов от ARWU сейчас?")) { // пользователь нажал 'Ok'
+        $('.pulse').trigger('click');
+      } else { // иначе - показать кнопку
+        $('.pulse').animate({
+          opacity: 1,
+        }, 300);
       }
     }
   });
